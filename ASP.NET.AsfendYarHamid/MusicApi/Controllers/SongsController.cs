2802a97296d1463b512455dffdf12f036ae29b20
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-03-24 21:07:25 +0100

    -

---
+++ ASP.NET.AsfendYarHamid/MusicApi/Controllers/SongsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicApi.Models;
+
+namespace MusicApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        private List<Song> songs = new List<Song>()
+        {
+            new Song
+            {
+                Id = 0, 
+                Title = "Willow", 
+                Language ="English"
+            },
+            new Song
+            {
+                Id = 1,
+                Title = "After Glow",
+                Language ="English"
+            }
+        };
+
+        [HttpGet]
+        public IEnumerable<Song> Get()
+        {
+            return songs;
+        }
+    }
+}
