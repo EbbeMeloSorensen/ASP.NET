a4842710b4fb54358f9c548d0f83c86ea17dfa01
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-04-15 10:25:01 +0200

    Added a call to a web api on the web - it seems to work

--- ASP.NET.PatrickGod/Visual Studio 2017/SimpleShoppingList/Scripts/app.js
+++ ASP.NET.PatrickGod/Visual Studio 2017/SimpleShoppingList/Scripts/app.js
@@ -1,3 +1,12 @@
 ﻿$(document).ready(function () {
-    console.info("ready");
+    console.info("before call to web api");
+    $.ajax({
+        url: "http://api.exchangeratesapi.io/v1/latest?access_key=3d5861a43985fbc3778eda7aaa2fefa9&symbols=USD,INR",
+        type: "GET",
+        dataType: "json",
+        success: function(result) {
+            console.info("after apparently receiving a response from the web api");
+        }
+    });
+    console.info("after call to web api");
 });
