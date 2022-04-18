598f7997b35296f4eab48436f82c97a2c6f3f9d8
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2022-04-18 10:53:25 +0200

    Working on SuperHero Manager

--- ASP.NET.PatrickGod/Visual Studio 2022/SuperHeroManager/wwwroot/js/site.js
+++ ASP.NET.PatrickGod/Visual Studio 2022/SuperHeroManager/wwwroot/js/site.js
@@ -1,4 +1,32 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var heroes = {};
 
-// Write your JavaScript code.
+function getAllHeroes() {
+    // Mocking for a start
+    heroes.items = new Array();
+
+    heroes.items = [
+        { id: 0, firstName: "Peter", lastName: "Parker", heroName: "Spiderman", placeOfBirth: "New York", combat: 85 },
+        { id: 1, firstName: "Bruce", lastName: "Wayne", heroName: "Batman", placeOfBirth: "Gotham City", combat: 100 }
+    ];
+
+    drawHeroTable(heroes);
+}
+
+function drawHeroTable(heroList) {
+    $tbody = $("#table-body");
+    $tbody.empty();
+    for (var i = 0; i < heroList.items.length; i++) {
+        $tr = $("<tr>");
+        $("<td>").html(heroList.items[i].firstName).appendTo($tr);
+        $("<td>").html(heroList.items[i].lastName).appendTo($tr);
+        $("<td>").html(heroList.items[i].heroName).appendTo($tr);
+        $("<td>").html(heroList.items[i].placeOfBirth).appendTo($tr);
+        $("<td>").html(heroList.items[i].combat).appendTo($tr);
+        $tbody.append($tr);
+    }
+}
+
+$(document).ready(function () {
+    console.info("ready");
+    getAllHeroes();
+});
