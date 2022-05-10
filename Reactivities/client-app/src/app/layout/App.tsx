716eb83f911e8e7d08ba322153fce11d0a9eccf9
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-05-10 18:54:05 +0200

    Added an activity interface for improved type safety (section 5, lesson 47)

--- Reactivities/client-app/src/app/layout/App.tsx
+++ Reactivities/client-app/src/app/layout/App.tsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { List, ListHeader } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
 
 function App() {
   // Initially make an empty list
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   // Populate the list with data retrieved from the api.
   // The empty array at the end is in order to avoid an infinite loop.
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities').then(response => {
+    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
       setActivities(response.data);
     })
   }, [])
@@ -18,7 +19,7 @@ function App() {
     <div>
       <ListHeader as='h2' content='Reactivities'/>
       <List>
-      {activities.map((activity: any) => (
+      {activities.map(activity => (
           <List.Item key={activity.id}>
             {activity.title}
           </List.Item>
