e6b1bc68ee5301230fec271965508f490eb21ef5
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-05-13 09:14:29 +0200

    End of section 3, where we used axios for fetching data from the API, populating the UI and styling it with Semantic UI React

--- Reactivities/client-app/src/App.tsx
+++ Reactivities/client-app/src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import { Header } from 'semantic-ui-react';
+import { Header, List } from 'semantic-ui-react';
+import axios from 'axios';
 
 function App() {
+  // Teorien for disse forklares fint i lesson 27
+  const [activities, setActivities] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
+      setActivities(response.data);
+    })
+  }, []) // Denne tomme liste er der for at undgå en uendelig løkke, som Neil beskriver
+
   return (
     <div>
-      <Header as='h2' content='It works :-)'/>
+      <Header as='h2' icon='users' content='Reactivities'/>
+        <List>
+        {activities.map((activity: any) => (
+            <List.Item key={activity.id}>
+              {activity.title}
+            </List.Item>
+          ))}
+        </List>
     </div>
   );
 }
