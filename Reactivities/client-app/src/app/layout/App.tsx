103b652688557d602e26116b47c9b7d15bab6100
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-05-18 19:20:40 +0200

    Completed lesson 62

--- Reactivities/client-app/src/app/layout/App.tsx
+++ Reactivities/client-app/src/app/layout/App.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Container } from 'semantic-ui-react';
-import axios from 'axios';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid';
+import agent from '../api/agent';
 
 function App() {
   // Teorien for disse forklares fint i lesson 27
@@ -13,8 +13,8 @@ function App() {
   const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
-      setActivities(response.data);
+    agent.Activities.list().then(response => {
+      setActivities(response);
     })
   }, []) // Denne tomme liste er der for at undgå en uendelig løkke, som Neil beskriver
 
