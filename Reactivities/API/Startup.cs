3af1f3daf97c8c8325f8ce0a93e2da2c6d8324a0
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-05-30 20:06:39 +0200

    Completed lesson 103

--- Reactivities/API/Startup.cs
+++ Reactivities/API/Startup.cs
@@ -1,4 +1,6 @@
+using FluentValidation.AspNetCore;
 using API.Extensions;
+using Application.Activities;
 
 namespace API
 {
@@ -14,7 +16,9 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddFluentValidation(config => {
+                config.RegisterValidatorsFromAssemblyContaining<Create>();
+            });
             services.AddApplicationServices(_config);
         }
 
