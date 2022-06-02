1b19321eb27aba162ed73104fa1b90bce0784060
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2022-06-02 18:43:12 +0200

    Completed lesson 108

--- Reactivities/API/Startup.cs
+++ Reactivities/API/Startup.cs
@@ -1,5 +1,6 @@
 using FluentValidation.AspNetCore;
 using API.Extensions;
+using API.Middleware;
 using Application.Activities;
 
 namespace API
@@ -25,14 +26,13 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
-
-            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
