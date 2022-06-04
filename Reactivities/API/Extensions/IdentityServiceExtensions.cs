6908bcff0f1f340cabc5d774a030e7d6f49de952
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-06-04 06:31:02 +0200

    Completed lesson 131

---
+++ Reactivities/API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,27 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using Domain;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(
+            this IServiceCollection services,
+            IConfiguration config)
+        {
+            services.AddIdentityCore<AppUser>(opt => {
+                opt.Password.RequireNonAlphanumeric = false;
+
+            })
+            .AddEntityFrameworkStores<DataContext>()
+            .AddSignInManager<SignInManager<AppUser>>();
+
+            services.AddAuthentication();
+
+            return services;
+        }
+    }
+}
