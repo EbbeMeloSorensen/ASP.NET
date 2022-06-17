a7568fb2894c60ba30e2d5bf9863ef6f5f97e18e
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-06-17 21:07:47 +0200

    Completed lesson 227

--- Reactivities/API/Controllers/FollowController.cs
+++ Reactivities/API/Controllers/FollowController.cs
@@ -13,8 +13,13 @@ namespace API.Controllers
         public async Task<IActionResult> Follow(string username)
         {
             return HandleResult(await Mediator.Send(new FollowToggle.Command{TargetUsername = username}));
-
         }
-       
+
+        [HttpGet("{username}")]
+        public async Task<IActionResult> GetFollowings(string username, string predicate)
+        {
+            return HandleResult(await Mediator.Send(new List.Query{Username = username,
+                Predicate = predicate}));
+        }
     }
 }
