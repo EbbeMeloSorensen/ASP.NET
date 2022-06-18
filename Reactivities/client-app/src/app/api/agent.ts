b1f503cc52571ee3fb958999c739a10a37f05236
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-06-18 06:36:10 +0200

    Completed lesson 231

--- Reactivities/client-app/src/app/api/agent.ts
+++ Reactivities/client-app/src/app/api/agent.ts
@@ -94,7 +94,9 @@ const Profiles = {
         })
     },
     setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
-    deletePhoto: (id: string) => requests.del(`/photos/${id}`)
+    deletePhoto: (id: string) => requests.del(`/photos/${id}`),
+    updateProfile: (profile: Partial<Profile>) => requests.put(`/profiles`, profile), // Tilføjet i lesson 231 - overså du den tidligere?
+    updateFollowing: (username: string) => requests.post(`/follow/${username}`, {})
 }
 
 const agent = {
